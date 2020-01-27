3772eb22492f1a946781a53989e6fae4243ea180
Urs Grupp <urs.grupp@gmail.com>
2020-01-27 16:43:30 +0100

    Linted cookieconsent script

--- src/js/modules/cookieconsent.js
+++ src/js/modules/cookieconsent.js
@@ -2,23 +2,23 @@ import 'cookieconsent';
 
 window.addEventListener('load', () => {
   window.cookieconsent.initialise({
-    "palette": {
-      "popup": {
-        "background": "#847465",
-        "text": "#ffffff"
+    'palette': {
+      'popup': {
+        'background': '#847465',
+        'text': '#ffffff',
       },
-      "button": {
-        "background": "#ffffff",
-        "text": "#847465"
-      }
+      'button': {
+        'background': '#ffffff',
+        'text': '#847465',
+      },
     },
-    "theme": "edgeless",
-    "position": "bottom-right",
-    "content": {
-      "message": "Wir verwenden Cookies. Durch die weitere Nutzung unserer Website stimmen Sie der Verwendung dieser Cookies zu.",
-      "dismiss": "Verstanden",
-      "link": "Mehr erfahren?",
-      "href": "https://www.hotel-peterle.de/datenschutz/"
-    }
+    'theme': 'edgeless',
+    'position': 'bottom-right',
+    'content': {
+      'message': 'Wir verwenden Cookies. Durch die weitere Nutzung unserer Website stimmen Sie der Verwendung dieser Cookies zu.',
+      'dismiss': 'Verstanden',
+      'link': 'Mehr erfahren?',
+      'href': 'https://www.hotel-peterle.de/datenschutz/',
+    },
   });
 });
